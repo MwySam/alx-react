2f60c73d9e4680f0c4f171ab1ef113223b2c0017
MwySam <samuelmwy@gmail.com>
2023-01-24 01:31:58 +0300

    webpack jquery install

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+import { $ } from "jquery";
+
+document.write = ("Holberton Dashboard");
+document.write = ("Dashboard data for the students");
+document.write = (" Copyright - Holberton School");
+
